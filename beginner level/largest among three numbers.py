cf7a900ed94b7c39249a203d6b3b9c35fbb333e7
jeyasoorya.m <32174464+jeyasoorya@users.noreply.github.com>
2018-02-23 15:36:43 +0530

    Create largest among three numbers.py

---
+++ beginner level/largest among three numbers.py
@@ -0,0 +1,9 @@
+a=input("enter no1:")
+b=input("enter no2:")
+c=input("enter no3:")
+if (a>b and a>c):
+    print "a is largest"
+elif(b>a and b>c):
+    print "b is largest"
+else:
+    print "c is largest"
